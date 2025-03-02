6124f7719af8fa72a3340bfff82ec841fa15e43f
dashbah <dsbakhankova@edu.hse.ru>
2025-03-02 16:47:42 +0300

    minor changes

--- wishlist-app/src/main/java/dashbah/wishlistapp/controller/HelloController.java
+++ wishlist-app/src/main/java/dashbah/wishlistapp/controller/HelloController.java
@@ -1,12 +1,24 @@
 package dashbah.wishlistapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Random;
+
 @Controller
+@Slf4j
 public class HelloController {
     @GetMapping("/")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        Random rnd = new Random();
+        int errorId = rnd.nextInt();
+        log.error("Error massage with id = " + errorId);
+        return "Error message with id = " + errorId;
     }
 }
